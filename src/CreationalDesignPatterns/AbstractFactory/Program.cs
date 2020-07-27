8d3268767cee668f4d85c961d2bb6787f3aef9e1
Ali Soltani <soltani1384@gmail.com>
2020-07-27 08:22:59 +0430

    adding Abstract Factory

---
+++ src/CreationalDesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFurnitureFactory modernfurnitureFactory = new ModernFurnitureFactory();
+            IFurnitureFactory victorianfurnitureFactory = new VictorianFurnitureFactory();
+        }
+    }
+
+    internal class VictorianFurnitureFactory : IFurnitureFactory
+    {
+        public IChair CreateChair() => new VictorianChair() { Material = "wood", Weight = 10 };
+
+        public ICoffeeTable CreateCoffeeTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISofa CreateSofa()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class VictorianChair : IChair
+    {
+        public string Material { get; set; }
+        public int Weight { get; set; }
+    }
+
+    internal class ModernFurnitureFactory : IFurnitureFactory
+    {
+        public IChair CreateChair()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICoffeeTable CreateCoffeeTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISofa CreateSofa()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal interface IFurnitureFactory
+    {
+        IChair CreateChair();
+        ICoffeeTable CreateCoffeeTable();
+        ISofa CreateSofa();
+    }
+
+    internal interface ISofa
+    {
+
+    }
+
+    internal interface ICoffeeTable
+    {
+
+    }
+
+    internal interface IChair
+    {
+        string Material { get; set; }
+        int Weight { get; set; }
+    }
+}
