c668f40bafe714459554041cdac4402312caab1e
Ali Soltani <soltani1384@gmail.com>
2020-09-14 15:08:57 +0430

    adding first behavioral design pattern

--- src/CreationalDesignPatterns/Singleton/Program.cs
+++ src/CreationalDesignPatterns/Singleton/Program.cs
@@ -6,7 +6,21 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            IShoppingApplications shoppingApplications = new MobileApplications();
             Console.WriteLine("Hello World!");
         }
+    }
+
+    internal class MobileApplications : IShoppingApplications
+    {
+        public void AddItem(string itemCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal interface IShoppingApplications
+    {
+        void AddItem(string itemCode);
     }
 }
